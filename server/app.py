713dfcaf0aecf04bdb092d6c95f4a9d84285bfe0
jhenilparihar <jhenilparihar@gmail.com>
2025-04-19 14:26:56 +0530

    port update to 5000

--- server/app.py
+++ server/app.py
@@ -119,4 +119,5 @@ def delete_url_chat(url_id):
 
 
 if __name__ == "__main__":
-    app.run(debug=app.config["DEBUG"])
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
